fd61515f29f1ae1b2e668578a1b1739d186ff2f4
daniel770770 <iamadevops@gmail.com>
2022-05-20 13:10:41 +0300

    This is the hangman game, enjoy!

---
+++ hangman.py
@@ -0,0 +1,25 @@
+#The hangman game 
+
+#intro screen
+
+print ("Welcome to the game Hangman")
+
+import random
+
+n = random.randint(4,10)
+>>> print(n)
+
+>>> HANGMAN_ASCII_ART = ("""Welcome to the game Hangman \n
+... ...   _    _
+... ...  | |  | |
+... ...  | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+... ...  |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \
+... ...  | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+... ...  |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+... ...                       __/ |
+... ...                      |___/""")
+
+
+
+
+
