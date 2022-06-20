f3cc716064b66152b833a1f39f4e081de99d9fe4
daniel770770 <iamadevops@gmail.com>
2022-06-20 22:34:56 +0300

    new ver

--- hangman.7.3.1.py
+++ hangman.7.3.1.py
@@ -4,18 +4,16 @@
 def show_hidden_word(secret_word, old_letters_guessed):
     list1=[]
     list1[:0]= secret_word
-    number_of_guesses= 4
+    u = " ".join(secret_word)
+    print (u)
+    number_of_guesses= 7
+
     while len(old_letters_guessed) < number_of_guesses : 
         x = input("Please guess a letter: ")
         old_letters_guessed.append(x)
-        print( old_letters_guessed)
-       # i = x
-        #for i in str(list1[::1]) : 
-         #   if i == str(list1[::1]) : 
-          #      y =  list1.replace(str(list1[::1]),"r")
-           #     print(y)
-            #else:
-             #   print("rowmg ansear")
+        y = list1.replace(
+        if x == list1 :
+            y = list1.replace(
 secret_word = "mammals"
 old_letters_guessed = []
 show_hidden_word(secret_word, old_letters_guessed)
